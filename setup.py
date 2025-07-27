a1d27a939fdba4454b9eadac02f8962f1ac4f1b6
Drue Staples <druestaples@Drues-MacBook-Pro.local>
2025-07-27 03:04:04 -0500

    updated python package

--- setup.py
+++ setup.py
@@ -1,31 +1,3 @@
-from setuptools import setup, find_packages
-from pathlib import Path
+from setuptools import setup
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
-setup(
-    name="footballmvp",  # lowercase recommended
-    version="0.1.0",
-    package_dir={"": "src"},             # tell setuptools to look in src/
-    packages=find_packages(where="src"),  # find packages only under src/
-    install_requires=[
-        "requests>=2.32",
-        "pandas>=2.2",
-        "numpy>=2.1",
-        "beautifulsoup4>=4.12",
-        "selenium>=4.32",
-        "pyspark>=3.5",
-    ],
-    author="Drue Staples",
-    author_email="druestaples@gmail.com",
-    description="Analytical tool to determine top performing football players (MVP) across leagues and tournaments.",
-    long_description=long_description,                     # <-- Add this
-    long_description_content_type='text/markdown',         # <-- And this for Markdown
-    url="https://github.com/DrueStaples08/FootballMVP",
-    classifiers=[
-        "Programming Language :: Python :: 3.12",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.12',
-)
+setup()
